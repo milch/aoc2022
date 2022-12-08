86f2fe8ca45c8c02c9fa2e88dcd847f9a23aeaee
Manu Wallner <manu@supermil.ch>
2022-12-08 08:39:58 -0800

    Solution for day 1

---
+++ day_01/src/main.rs
@@ -0,0 +1,33 @@
+const INPUT: &str = include_str!("../input.txt");
+
+fn convert_input_to_array<'a>(input: &str) -> Vec<Vec<i32>> {
+    input
+        .lines()
+        .fold(vec![vec![]], |mut accum: Vec<Vec<i32>>, line: &str| {
+            if line.is_empty() {
+                accum.push(vec![]);
+            } else {
+                let last = accum.last_mut().unwrap();
+                let current_value: i32 = line.parse().unwrap();
+                last.push(current_value);
+            }
+            accum
+        })
+}
+
+fn main() {
+    // let mut max_n = [0, -1, -2];
+    let result = convert_input_to_array(INPUT);
+    let mut sums: Vec<i32> = result
+        .iter()
+        .map(|carrying| carrying.iter().fold(0, |sum, &i| sum + i))
+        .collect();
+    let single_max = sums
+        .iter()
+        .reduce(|max, i| if i > max { i } else { max })
+        .unwrap();
+    println!("Max: {:?}", single_max);
+    sums.sort_unstable_by(|a, b| b.cmp(a));
+    let highest_three = sums.as_slice().get(0..3).unwrap();
+    println!("Max 3: {:?}", highest_three.iter().sum::<i32>())
+}
